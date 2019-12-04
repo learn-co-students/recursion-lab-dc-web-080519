70adce264d0b6564f9049f4c88bc030b194e3b6c
Pete Hanner <petehanner@gmail.com>
2019-12-04 10:32:00 -0500

    example problem

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0])
+  if (string.length > 1) {
+    const subString = string.substring(1, string.length - 1);
+    printString(subString);
+  } else {
+    return true;
+  }
+}
