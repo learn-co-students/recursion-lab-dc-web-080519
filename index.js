9c766d1f9f23b34da1f834da738e5eefb00a0735
Pete Hanner <petehanner@gmail.com>
2019-12-04 12:41:48 -0500

    Reversed string w different solution but works

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // Code your solution here!
 
-function printString(string) {
-  console.log(string[0])
-  if (string.length > 1) {
-    const subString = string.substring(1, string.length - 1);
+function printString(str) {
+  console.log(str[0])
+  if (str.length > 1) {
+    const subString = str.substring(1, str.length);
     printString(subString);
   } else {
     return true;
+  }
+}
+
+function reverseString(str) {
+  console.log(str.slice(-1))
+  if (str.length > 1) {
+    const subString = str.substring(0, str.length - 1)
+    reverseString(subString)
+  } else {
+    return true
   }
 }
